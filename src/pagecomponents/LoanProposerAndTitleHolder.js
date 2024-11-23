d56ae984b95f8b8a090b8f3b96243eac217e62f6
praveen238 <durgapraveen238@gmail.com>
2024-11-23 07:22:11 +0530

    font sizes changed

--- src/pagecomponents/LoanProposerAndTitleHolder.js
+++ src/pagecomponents/LoanProposerAndTitleHolder.js
@@ -7,15 +7,15 @@ const LoanProposerAndTitleHolder =({onOptionSelect})=>{
 const navigate = useNavigate(); // Initialize the navigate function
 
     return(
-        <div className="d-flex flex-column justify-content-center align-items-center" style={{marginTop:"0px"}} >
-           <h3>is the title holder  
-           same as the loan proposer?
+        <div className="d-flex flex-column justify-content-center align-items-center" style={{marginTop:"200px"}} >
+           <h3>Is The Title Holder  
+           Same As The loan Proposer?
            </h3>
            <div className="d-flex justify-content-center">
              {/* YES button navigates to a specific page */}
             {/* <Button onClick={() => navigate('/MostRecentDocuments')}> YES</Button> */}
-            <Button   onClick={() => onOptionSelect("YES")}>YES</Button>
-            <Button> NO</Button>
+            <Button style={{width:"100px" , fontWeight:"bold", fontSize:"20px"}} className="mx-3"  onClick={() => onOptionSelect("YES")}>YES</Button>
+            <Button style={{width:"100px", fontWeight:"bold", fontSize:"20px"}}> NO</Button>
            </div>
         </div>
     )
