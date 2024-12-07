b9c9a2542ba55a1c47e8c28177976e7e8b5ab891
Srikanth <srikanthbalagam777@gmail.com>
2024-12-07 23:51:30 +0530

    property detsils page

--- src/pagecomponents/Signup.js
+++ src/pagecomponents/Signup.js
@@ -1,15 +1,16 @@
 import { useFormik } from "formik";
 import React from "react";
-import { Link } from "react-router-dom";
+ 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/esm/Button';
 
-const   Login =()=>{
+const  Signup =()=>{
    const  formik = useFormik({
       initialValues:{
         name:"",
         email:"",
-        password:""
+        password:"",
+        confirmPassword:""
       },
       onSubmit:(values)=>{
         console.log("form submit", formik.values)
@@ -26,23 +27,31 @@ const   Login =()=>{
           errors.email = "Invalid email address";
         }
         
-        if (!values.name){   
-          errors.password= "Password is required"
-        }else if (values.password.length < 6) {
-        errors.password = "Password must be at least 6 characters";
+         
+        // Password Validation
+        if (!values.password) {
+          errors.password = "Password is required";
+        } else if (values.password.length < 6) {
+          errors.password = "Password must be at least 6 characters";
         }
+
+        // Confirm Password Validation
+        if (!values.confirmPassword) {
+          errors.confirmPassword = "Confirm Password is required";
+        } else if (values.password && values.password.length >= 6) {
+          if (values.password !== values.confirmPassword) {
+            errors.confirmPassword = "Passwords do not match";
+          }
+        }
+
       return errors;
 
       }
 
    })
-    
-
-
-
     return(
         <div>
-            <h1 className="text-center text-primary"   > Login   </h1>   {/*just here creating another page */}
+            <h1 className="text-center text-primary"> Signup   </h1>   {/*just here creating another page */}
 
               <div className="d-flex justify-content-center"  > 
               <div className="col col-md-6 col-lg-10" 
@@ -58,26 +67,41 @@ const   Login =()=>{
                     <Form.Group className="mb-3" controlId="forname">
                         <Form.Label className="fs-4">Name </Form.Label>
                         <Form.Control   type="text" placeholder="Enter name" name="name"  
-                         value={formik.values.name} onChange={formik.handleChange}/>
+                         value={formik.values.name}
+                         onChange={formik.handleChange}
+                         onBlur={formik.handleBlur} />
                          {formik.errors.name?<div className="text-danger">{formik.errors.name}</div>:null}
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label className="fs-4">Email </Form.Label>
                         <Form.Control type="email" placeholder="Enter email" name="email"
-                         value={formik.values.email} onChange={formik.handleChange}/>
+                         value={formik.values.email} 
+                         onChange={formik.handleChange}
+                         onBlur={formik.handleBlur}/>
                          {formik.errors.email?<div className="text-danger">{formik.errors.email}</div>:null}
                     </Form.Group>
  
-                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Group className="mb-3" controlId="Password">
                         <Form.Label className="fs-4">Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" name="password"
-                         value={formik.values.password} onChange={formik.handleChange}/>
+                         value={formik.values.password}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}/>
                          {formik.errors.password?<div className="text-danger">{formik.errors.password}</div>:null}
                     </Form.Group>
                     
-                     <p>If you don't have an account  <Link to="/Signup">signup</Link> here</p>
-                    <Button type="submit"> Login</Button>
+                    <Form.Group className="mb-3" controlId="confirmPassword">
+                        <Form.Label className="fs-4">confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="confirmPassword" name="confirmPassword"
+                         value={formik.values.confirmPassword}
+                          onChange={formik.handleChange} 
+                          onBlur={formik.handleBlur}/>
+                         {formik.errors.confirmPassword?<div className="text-danger">{formik.errors.confirmPassword}</div>:null}
+                    </Form.Group>
+                    
+                     {/* <p> If you have an account   <Link to="/Login">Login</Link> here</p>   */}
+                    <Button type="submit"> Signup</Button>
                 </Form>  
                    
               </div>
@@ -87,4 +111,4 @@ const   Login =()=>{
     )
 }
 
-export default   Login;
+export default  Signup;
