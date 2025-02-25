168677697f8148e486b4b1cc6a2d8d84c0236831
sandeepnaidu666 <sandeeppilla666@gmail.com>
2025-02-25 21:44:24 +0530

    environment_variable

--- src/apiUrls.js
+++ src/apiUrls.js
@@ -1,9 +1,10 @@
-import { dns } from "./dns"
+import { dns } from "./dns.env"
+//import { dns } from './dnsConfig';
 
-export const LoanProposerDetails_api = dns +"/api/submit-boundaries"
-export const Login_api = dns +"/api/submit-boundaries"
-export const Mostrecentdocuments_api = dns +"/api/submit-boundaries"
-export const Propertyboundaries_api = dns +"/api/submit-boundaries"
-export const Propertydetails_api = dns +"/api/submit-boundaries"
-export const Signup_api = dns +"/api/submit-boundaries"
-export const TitleHolderDetails_api = dns +"/api/submit-boundaries"
+export const LoanProposerDetails_api = {dns} +"/api/submit-boundaries"
+export const Login_api = {dns} +"/api/submit-boundaries"
+export const Mostrecentdocuments_api = {dns} +"/api/submit-boundaries"
+export const Propertyboundaries_api = {dns} +"/api/submit-boundaries"
+export const Propertydetails_api = {dns} +"/api/submit-boundaries"
+export const Signup_api = {dns} +"/api/submit-boundaries"
+export const TitleHolderDetails_api = {dns} +"/api/submit-boundaries"
