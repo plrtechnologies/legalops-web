6db459e891d9babcd3802c28e7b944687597aec8
sandeepnaidu666 <sandeeppilla666@gmail.com>
2025-03-08 16:24:28 +0530

    .env file creation

--- src/apiUrls.js
+++ src/apiUrls.js
@@ -1,9 +1,9 @@
-import { dns } from "./dns"
+import dnsData from "./dns.json";
 
-export const LoanProposerDetails_api = dns +"/api/submit-boundaries"
-export const Login_api = dns +"/api/submit-boundaries"
-export const Mostrecentdocuments_api = dns +"/api/submit-boundaries"
-export const Propertyboundaries_api = dns +"/api/submit-boundaries"
-export const Propertydetails_api = dns +"/api/submit-boundaries"
-export const Signup_api = dns +"/api/submit-boundaries"
-export const TitleHolderDetails_api = dns +"/api/submit-boundaries"
+export const LoanProposerDetails_api = dnsData.dns + "/api/submit-boundaries";
+export const Login_api = dnsData.dns + "/api/submit-boundaries";
+export const Mostrecentdocuments_api = dnsData.dns + "/api/submit-boundaries";
+export const Propertyboundaries_api = dnsData.dns + "/api/submit-boundaries";
+export const Propertydetails_api = dnsData.dns + "/api/submit-boundaries";
+export const Signup_api = dnsData.dns + "/api/submit-boundaries";
+export const TitleHolderDetails_api = dnsData.dns + "/api/submit-boundaries";
